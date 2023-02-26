a7f475cf6d39613be8c5e212c0cf97a878180c14
Apoorve Verma <vapoorve@gmail.com>
2023-02-27 02:14:48 +0530

    BGChanger: Button Created and added some styling

--- bgChanger/App.tsx
+++ bgChanger/App.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
 const App = () => {
   return (
     <>
-      <Text>Welcome to My App</Text>
+      <View style={styles.container}>
+        <TouchableOpacity>
+          <Text style={styles.text}>Click me</Text>
+        </TouchableOpacity>
+      </View>
     </>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rgb(32,0,126)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    backgroundColor: '#BB2CD9',
+    color: '#fff',
+    fontSize: 24,
+    paddingHorizontal: 40,
+    paddingVertical: 10,
+    borderRadius: 15,
+    textTransform: 'uppercase',
+  },
+});
