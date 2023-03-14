27215f8863b68014b0697fcd9194860dfbecefba
Apoorve Verma <vapoorve@gmail.com>
2023-03-14 22:21:41 +0530

    Update

--- reduxTodoApp/action/action.types.js
+++
@@ -1,3 +0,0 @@
-export const ADD_TASK = 'ADD_TASK';
-export const REMOVE_TASK = 'REMOVE_TASK';
-export const IS_COMPLETE_TASK = 'IS_COMPLETE_TASK';
--- reduxTodoApp/redux/store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import rootTodoReducer from './todoSlice';
-
-export const store = configureStore({
-  reducer: {
-    todo: rootTodoReducer,
-  },
-});
